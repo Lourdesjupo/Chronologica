a32b24da66ed0aa62dbd9c134bba416e6c7f0e7f
Yanelis Serrano <yaneliserranotaylor@gmail.com>
2023-07-11 09:39:56 +0200

    Improves and fix add task

--- src/components/AddTask_OneTime.jsx
+++ src/components/AddTask_OneTime.jsx
@@ -1,133 +1,104 @@
-import { Box, Fab, Grid, Stack, TextField } from '@mui/material';
+import { Box, Fab, Grid, Stack, TextField } from "@mui/material";
 /* eslint-disable react/prop-types */
-import PetsIcon from '@mui/icons-material/Pets';
-import YardIcon from '@mui/icons-material/Yard';
-import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
-import AutoStoriesIcon from '@mui/icons-material/AutoStories';
-import MedicationIcon from '@mui/icons-material/Medication';
-import FreeBreakfastIcon from '@mui/icons-material/FreeBreakfast';
-import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
-import LocalAtmIcon from '@mui/icons-material/LocalAtm';
-import SchoolIcon from '@mui/icons-material/School';
-import EventRepeatIcon from '@mui/icons-material/EventRepeat';
-import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
-import HeadphonesBatteryIcon from '@mui/icons-material/HeadphonesBattery';
-import EditNoteIcon from '@mui/icons-material/EditNote';
-import ChecklistIcon from '@mui/icons-material/Checklist';
-import LocalDrinkIcon from '@mui/icons-material/LocalDrink';
-import { useState } from 'react';
+import PetsIcon from "@mui/icons-material/Pets";
+import YardIcon from "@mui/icons-material/Yard";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import MedicationIcon from "@mui/icons-material/Medication";
+import FreeBreakfastIcon from "@mui/icons-material/FreeBreakfast";
+import SelfImprovementIcon from "@mui/icons-material/SelfImprovement";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+import SchoolIcon from "@mui/icons-material/School";
+import EventRepeatIcon from "@mui/icons-material/EventRepeat";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import HeadphonesBatteryIcon from "@mui/icons-material/HeadphonesBattery";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import ChecklistIcon from "@mui/icons-material/Checklist";
+import LocalDrinkIcon from "@mui/icons-material/LocalDrink";
+import { useState } from "react";
 
 function AddTask_OneTime() {
-  const [icon, setIcon] = useState([{id:'PetsIcon', state: true},{id:'LocalDrinkIcon', state: true},{id:'ChecklistIcon', state: true}])
-  const handleClick =(ev)=>{
-        const id = ev.currentTarget.id;
-        icon[id].state = !icon[id].state
-        setIcon([...icon],)
-        console.log(id)
-        
-        
-      }
-      console.log(icon['PetsIcon'].state)
-      console.log(icon.find((item)=>item.id ==='PetsIcon') )
+  const [icons, setIcons] = useState([
+    { id: "PetsIcon", state: true },
+    { id: "YardIcon", state: true },
+    { id: "AutoStoriesIcon", state: true },
+    { id: "MedicationIcon", state: true },
+    { id: "FreeBreakfastIcon", state: true },
+    { id: "SelfImprovementIcon", state: true },
+    { id: "SchoolIcon", state: true },
+    { id: "EventRepeatIcon", state: true },
+    { id: "LocalPhoneIcon", state: true },
+    { id: "HeadphonesBatteryIcon", state: true },
+    { id: "EditNoteIcon", state: true },
+    { id: "ChecklistIcon", state: true },
+    { id: "LocalDrinkIcon", state: true },
+    { id: "LocalDrinkIcon", state: true }
+
+  ]);
+
+  const handleClick = (ev) => {
+    const id = ev.currentTarget.id;
+    const foundIndexIcon= icons.findIndex(item=>item.id === id); 
+    icons[foundIndexIcon].state = !icons[foundIndexIcon].state; 
+    setIcons([...icons]);
+  };
+
+  const rendersIcons=()=>{
+    return icons.map((item, index)=>{
+      return <Grid key={index} item xs={0}>
+            <Fab id={item.id} 
+              onClick={handleClick} 
+              color={icons.find(item=>item.id ===item.id).state === true ? 'primary' : 'default'}>
+              { 
+                item.id === 'PetsIcon'? <PetsIcon fontSize="large" /> : 
+                item.id === 'LocalDrinkIcon'? <LocalDrinkIcon fontSize="large" /> : 
+                item.id === 'ChecklistIcon'? <ChecklistIcon fontSize="large" /> :
+                item.id === 'EditNoteIcon'? <EditNoteIcon fontSize="large" /> : 
+                item.id === 'YardIcon'? <YardIcon fontSize="large" /> : 
+                item.id === 'FitnessCenterIcon'? <FitnessCenterIcon fontSize="large" /> : 
+                item.id === 'AutoStoriesIcon'? <AutoStoriesIcon fontSize="large" /> :
+                item.id === 'MedicationIcon'? <MedicationIcon fontSize="large" /> : 
+                item.id === 'FreeBreakfastIcon'? <FreeBreakfastIcon fontSize="large" /> :
+                item.id === 'SelfImprovementIcon'? <SelfImprovementIcon fontSize="large" /> : 
+                item.id === 'LocalAtmIcon'? <LocalAtmIcon fontSize="large" /> : 
+                item.id === 'SchoolIcon'? <SchoolIcon fontSize="large" /> : 
+                item.id === 'EventRepeatIcon'? <EventRepeatIcon fontSize="large" /> : 
+                item.id === 'LocalPhoneIcon'? <LocalPhoneIcon fontSize="large" /> : 
+                item.id === 'HeadphonesBatteryIcon'? <HeadphonesBatteryIcon fontSize="large" /> : 
+                item.id === 'FreeBreakfastIcon'? <FreeBreakfastIcon fontSize="large" /> : ''
+              }
+            </Fab>
+          </Grid>
+    }); 
+   
+  }
+
   return (
     <>
       <Box
-        component='form'
+        component="form"
         sx={{
-          '& > :not(style)': { m: 4, width: '50ch' },
+          "& > :not(style)": { m: 4, width: "50ch" },
         }}
-        autoComplete='off'
+        autoComplete="off"
       >
         <TextField
-          id='outlined-basic'
-          label='Nombre de la Tarea'
-          variant='outlined'
+          id="outlined-basic"
+          label="Nombre de la Tarea"
+          variant="outlined"
         />
       </Box>
-      <Stack component='ul' sx={{ mr: 15 }}>
+      <Stack component="ul" sx={{ mr: 15 }}>
         <Grid
           container
           spacing={3}
-          direction='row'
-          flexWrap='wrap'
+          direction="row"
+          flexWrap="wrap"
           sx={{ mx: 1, mt: 4 }}
-          component='li'
+          component="li"
         >
-          <Grid item xs={0}>
-            <Fab id="PetIcon" onClick={handleClick} color={icon.find((item)=>item.id ==='PetsIcon').state === true ? 'primary' : 'secondary' }>
-              {<PetsIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<YardIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<FitnessCenterIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<AutoStoriesIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<MedicationIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<FreeBreakfastIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<SelfImprovementIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<LocalAtmIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<SchoolIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<EventRepeatIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<LocalPhoneIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<HeadphonesBatteryIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<EditNoteIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<ChecklistIcon fontSize='large' />}
-            </Fab>
-          </Grid>
-          <Grid item xs={0}>
-            <Fab onClick={handleClick} color='secondary'>
-              {<LocalDrinkIcon fontSize='large' />}
-            </Fab>
-          </Grid>
+          {rendersIcons()}
+   
         </Grid>
       </Stack>
     </>
