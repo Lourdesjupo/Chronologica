2cef256bc9bc66ad480bd4191cd6d0279b1dd3a3
Lourdes Juárez Porto <lourdes.jupo@gmail.com>
2024-03-25 20:19:44 +0100

    Add themeProvider for a circularProgress component

--- src/components/TrackedTimeReport.jsx
+++ src/components/TrackedTimeReport.jsx
@@ -1,14 +1,13 @@
-
-import Box from '@mui/material/Box';
-import { DataGrid,GridToolbarContainer,GridToolbarExport  } from '@mui/x-data-grid';
-import { useEffect, useState } from 'react';
-import { getTrackedListTasks } from '../services/ApiTrackedTime';
-import { CircularProgress, Typography } from '@mui/material';              
-
-
-
-
-
+import Box from "@mui/material/Box";
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarExport,
+} from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import { getTrackedListTasks } from "../services/ApiTrackedTime";
+import { Typography } from "@mui/material";
+import { CircularProgress, ThemeProvider, extendTheme } from '@mui/joy';
 
 function CustomToolbar() {
   return (
@@ -18,115 +17,130 @@ function CustomToolbar() {
   );
 }
 
-
-
 function TrackedTimeReport() {
+  const [trackedList, setTrackedList] = useState([]);
 
-  const [trackedList,setTrackedList]= useState([])
-
-  useEffect(()=>{
-    getTrackedListTasks().then((data)=>{
-      data.map((el)=>{
-        el.elapsedTime = Math.round(el.elapsedTime)
-        if(el.estimatedTime){  
-          el.remain = Math.round(el.estimatedTime - el.elapsedTime)
-          el.progress = Math.round((el.elapsedTime * 100) /el.estimatedTime)
+  useEffect(() => {
+    getTrackedListTasks().then((data) => {
+      data.map((el) => {
+        el.elapsedTime = Math.round(el.elapsedTime);
+        if (el.estimatedTime) {
+          el.remain = Math.round(el.estimatedTime - el.elapsedTime);
+          el.progress = Math.round((el.elapsedTime * 100) / el.estimatedTime);
         }
-      })
-    setTrackedList(data)
-    console.log('reportData:', data)
-    })
-  },[])
+      });
+      setTrackedList(data);
+      console.log("reportData:", data);
+    });
+  }, []);
 
- 
-  const renderProgress = (p)=>{ 
-    console.log(p, 'manola: ', p.row.color)
-  if(!p.row.estimatedTime){
-    return 
-  } else{
-    return  (
-      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-        <CircularProgress variant="determinate" color='secondary' {...p} />
-        <Box
-          sx={{
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography variant="caption" component="div" color='secondary'>
-            {`${Math.round(p.row.progress)}%`}
-          </Typography>
+  const renderProgress = (p) => {
+    if (!p.row.estimatedTime) {
+      return;
+    } else {
+      return (
+        <Box sx={{ position: "relative", display: "inline-flex" }}>
+          <ThemeProvider theme={theme2}>
+          <CircularProgress
+            variant="soft"
+            determinate
+            color="success"
+            value={50}
+          />
+          </ThemeProvider>
+          <Box
+            sx={{
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              position: "absolute",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="caption" component="div" color="secondary">
+              {`${Math.round(p.row.progress)}%`}
+            </Typography>
+          </Box>
         </Box>
-      </Box>
-    );
-    //<CircularProgress variant="determinate" value={p.row.progress} />
-  }
-  
-  }
- //dedicación media por sesión, nº de días diferentes dedicados. En el último mes X , en el ultimo dos meses XX
-  const columns = [  
-  {
-    field: 'name',
-    headerName: 'Nombre de tarea',
-    width: 200,
-    editable: false,
-  },
-  {
-    field: 'elapsedTime',
-    headerName: 'Minutos dedicados',
-    type: 'number',
-    width: 200,
-    headerAlign: 'center',
-    editable: false,
-  },
-  {
-    field: 'remain',
-    headerName: 'Minutos restantes',
-    type: 'number',
-    width: 200,
-    headerAlign: 'center',
-    editable: false,
-  },
-  {
-    field: 'progress',
-    headerName: 'Progreso',
-    renderCell: renderProgress,
-    headerAlign: 'center',
-    width: 200,
-  }
-];
+      );
+      //<CircularProgress variant="determinate" value={p.row.progress} />
+    }
+  };
+  //dedicación media por sesión, nº de días diferentes dedicados. En el último mes X , en el ultimo dos meses XX
+  const columns = [
+    {
+      field: "name",
+      headerName: "Nombre de tarea",
+      width: 200,
+      headerAlign: "center",
+      align: "center",
+      editable: false,
+    },
+    {
+      field: "elapsedTime",
+      headerName: "Minutos dedicados",
+      type: "number",
+      width: 200,
+      headerAlign: "center",
+      align: "center",
+      editable: false,
+    },
+    {
+      field: "remain",
+      headerName: "Minutos restantes",
+      type: "number",
+      width: 200,
+      headerAlign: "center",
+      align: "center",
+      editable: false,
+    },
+    {
+      field: "progress",
+      headerName: "Progreso",
+      renderCell: renderProgress,
+      headerAlign: "center",
+      align: "center",
+      width: 200,
+    },
+  ];
+  const theme2 = extendTheme({
+    components: {
+      JoyCircularProgress: {
+        defaultProps: {
+          variant: 'soft',
+          color: 'success',
+        },
+        
+      },
+    },
+  });
 
-
-return( <Box sx={{ width: '100%', height:500}}>
-
-
-
+  return (
+    <Box sx={{ width: "100%", height: 500 }}>
+      
       <DataGrid
-        slots={{toolbar: CustomToolbar }}          
+        slots={{ toolbar: CustomToolbar }}
         rows={trackedList}
         columns={columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize:10,
+              pageSize: 10,
             },
           },
         }}
-        sx={{ '--DataGrid-overlayHeight': '300px'}}
+        //sx={{ '--DataGrid-overlayHeight': '300px'}}
         checkboxSelection={false}
         disableRowSelectionOnClick
         pageSizeOptions={[10]}
-        density={'comfortable'}
+        density={"comfortable"}
       />
-    
+     
     </Box>
   );
 }
 
-export default TrackedTimeReport
+export default TrackedTimeReport;
--- src/main.jsx
+++ src/main.jsx
@@ -1,63 +1,61 @@
-
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-//import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 // Supports weights 100-900
-import '@fontsource-variable/raleway';
+import "@fontsource-variable/raleway";
 // Supports weights 300-700
-import '@fontsource-variable/quicksand';
+import "@fontsource-variable/quicksand";
 // import '@fontsource/roboto/400.css';
 // import '@fontsource/roboto/500.css';
 // import '@fontsource/roboto/700.css';
 
-import { createTheme, ThemeProvider  } from '@mui/material/styles';
-
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
-  typography: {
-    fontFamily: 'Quicksand',
-    
-  },
+  // typography: {
+  //   fontFamily: 'Quicksand',
+
+  // },
   palette: {
-     type: 'light',
+    type: "light",
     primary: {
-      main: '#9684c4',
-     /// main: #6750A4,
+      main: "#9684c4",
+      /// main: #6750A4,
     },
     secondary: {
-      main: '#625B71',
+      main: "#625B71",
     },
-    secondary_container:{
-      main: '#E8DEF8'
+    secondary_container: {
+      main: "#E8DEF8",
     },
-    tertiary:{
-      main: '#7D5260'
+    tertiary: {
+      main: "#7D5260",
     },
     success: {
-      main: '#81c784',
+      main: "#81c784",
     },
     error: {
-      main: '#B3261E',
+      main: "#B3261E",
     },
-    background:{
-      main:'#FFFBFE'
+    background: {
+      main: "#FFFBFE",
     },
-    onbackground:{
-     main:'#1C1B1F'
+    onbackground: {
+      main: "#1C1B1F",
     },
-    outline:{
-     main:'#79747E'
+    outline: {
+      main: "#79747E",
     },
     warning: {
-      main: '#e8c85f',
+      main: "#e8c85f",
     },
-  
   },
 });
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-,
-)
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
